98e197578c8b962ac1f6d6e24c2ab46813a403d3
Ken Diehl <diehlkj.99@gmail.com>
2020-09-16 21:46:02 -0400

    Completed Node class and LinkedList class except get_max method. Tests passing

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,7 +1,7 @@
 class Node:
-    def __init__(self, value=None, next_node=None)
-    self.value = value
-    self.next_node = next_node
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
 
     def get_value(self):
         return self.value
@@ -37,12 +37,13 @@ class LinkedList:
     def add_to_tail(self, value):
         # * Create new node
         new_node = Node(value)
+        
         # * Empty list condition
-        if self.head is None: 
+        if self.head is None:
             # * Update head and tail attributes
             self.head = new_node
             self.tail = new_node
-            
+
         # * Non empty list condition
         else:
             # * Update next_node of current tail to new node
@@ -51,50 +52,54 @@ class LinkedList:
             self.tail = new_node
 
     def remove_head(self):
-        previous_head = self.head.get_value()  # ! Return value of previous head
 
         # * Empty head condition
-        if self.head = None:
-            return previous_head
+        if self.head is None:
+            return None
 
-        # * List with 1 elem condition
-        elif self.head == self.tail:
-            self.head = None
-            self.tail = None
-
-        # * List with 2+ elem condition
         else:
-            self.head = self.head.get_next_node()
-        
-        return previous_head
+            prev_head = self.head.get_value()
+
+            # * List with 1 elem condition
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+
+            # * List with 2+ elem condition
+            else:
+                self.head = self.head.get_next_node()
+
+            return prev_head
 
     def remove_tail(self):
-        previous_tail = self.tail.get_value()  # ! NOT COMPLETE, GET IT DONE
 
-        # * Empty head condition
-        if self.tail = None:
-            return previous_head
+        # * Empty Tail condition
+        if self.tail is None:
+            return None
 
-        # * List with 1 elem condition
-        elif self.head == self.tail:
-            self.head = None
-            self.tail = None
-
-        # * List with 2+ elem condition
         else:
-            self.head = self.head.get_next_node()
-        
-        return previous_head
+            prev_tail = self.tail.get_value()
+
+            # * List with 1 elem condition
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+
+            # * List with 2+ elem condition
+            else:
+                self.tail = self.head
+
+        return prev_tail
 
     def contains(self, value):
         current_node = self.head
-        
+
         # * Loop through LL untill pointer is None
         while current_node.get_next_node() is not None:
             # * If value found
             if current_node.get_value() == value:
                 return True
-            
+
         return False
 
     def get_max(self):
