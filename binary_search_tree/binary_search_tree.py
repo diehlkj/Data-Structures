2d8ec94e4040714e3220a269fffa7ce61d38e0a3
Ken Diehl <diehlkj.99@gmail.com>
2020-09-21 23:27:46 -0400

    BSTNode insert complete and tested passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -3,6 +3,9 @@ Binary search trees are a data structure that enforce an ordering over
 the data they store. That ordering in turn makes it a lot more efficient 
 at searching for a particular piece of data in the tree. 
 
+! value < node is stored left
+! value >= node is stored right
+
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
    on the BSTNode class.
@@ -17,19 +20,63 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if self.left is None:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)
+
+        if value >= self.value:
+            if self.right is None:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+    
+    # ! Recursive Example:
+        # ? if value < Root
+            # ? if left child is None
+                # * insert here
+            # ? else
+                # * call insert on next tree
+        # ? if value >= Root, go right
+            # ? if right child is None
+                # * insert here
+            # ? else
+                # * call insert on next tree
+                
+    # ! Iterative Example:
+        # ? While not at end of tree
+            # ? if value < Root, go left
+                # ? if left child is none
+                    # * exit loop
+                    # * insert here
+
+            # ? if value >= Root, go right
+                # ? if right child is none
+                    # * exit loop
+                    # * insert here
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
+        # ? if self.value is target
+            # ? if yes 
+                # * return True
+            # ? if no
+                # ? go left
+                # ? go right
+            
         pass
 
     # Return the maximum value found in the tree
     def get_max(self):
+        # ? Go right until right is None
+            # * Return the value of final branch of right
         pass
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
+        # ? Once side then the other...
         pass
 
     # Part 2 -----------------------
@@ -63,23 +110,23 @@ class BSTNode:
 """
 This code is necessary for testing the `print` methods
 """
-bst = BSTNode(1)
+# bst = BSTNode(1)
 
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
+# bst.insert(8)
+# bst.insert(5)
+# bst.insert(7)
+# bst.insert(6)
+# bst.insert(3)
+# bst.insert(4)
+# bst.insert(2)
 
-bst.bft_print()
-bst.dft_print()
+# bst.bft_print()
+# bst.dft_print()
 
-print("elegant methods")
-print("pre order")
-bst.pre_order_dft()
-print("in order")
-bst.in_order_dft()
-print("post order")
-bst.post_order_dft()  
+# print("elegant methods")
+# print("pre order")
+# bst.pre_order_dft()
+# print("in order")
+# bst.in_order_dft()
+# print("post order")
+# bst.post_order_dft()  
